e5e0dcb3165974572f7a5fc89c5bf69d1ffde504
Ashi-Verma1758 <ashiverma1758@gmail.com>
2024-12-31 19:54:26 +0530

    modal

--- src/Components/Navbar/nav.jsx
+++ src/Components/Navbar/nav.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import './nav.css';
 import { useRef } from 'react';
 import DIT from '../../assets/DIT.png';
@@ -9,8 +10,14 @@ import { GiHamburgerMenu } from "react-icons/gi";
 import { Link } from "react-scroll";
 import Gallery_Button from '../Gallery_Button/Gallery_Button';
 import { FaCaretDown } from "react-icons/fa";
+import image from "../../assets/yellowline.png";
 
 function Nav() {
+    const [isModalVisible, setModalVisible] = useState(false);
+
+    const toggleModal = () => {
+        setModalVisible(!isModalVisible);
+    };
     const navRef = useRef();
 
     const showNav = () => {
@@ -31,19 +38,13 @@ function Nav() {
                 <div className="center-button">
                     <Link to="home" offset={-80}><li onClick={showNav}>HOME</li></Link>
                     <Link to="" offset={-80}><li onClick={showNav} >ARCHIVE<FaCaretDown /> 
-                        <ul className="dropdown">
-                            <div className='content'><a href="#">2024</a></div>
-                            <div className='content'><a href="#">2023</a></div>
-                            <div className='content'><a href="#">2022</a></div>
-                            <div className='content'><a href="#">2021</a></div>
-                            <div className='content'><a href="#">2020</a></div>
-                            <div className='content'><a href="#">2019</a></div>
-                            <div className='content'><a href="#">2018</a></div>
-                            <div className='content'><a href="#">2017</a></div>
-                            <div className='content'><a href="#">2016</a></div>
-                            <div className='content'><a href="#">2015</a></div>
-                            <div className='content'><a href="#">2014</a></div>
-                            </ul></li></Link>
+                    <ul className="dropdown">
+                            {["2024", "2023", "2022", "2021", "2020", "2019", "2018", "2017", "2016", "2015", "2014"].map(year => (
+                                <div key={year} className='content'>
+                                    <a href="#" onClick={toggleModal}>{year}</a>
+                                </div>
+                            ))}
+                        </ul></li></Link>
                     <Link to="nav-sports" offset={-80}><li onClick={showNav}>SPORTS</li></Link>
                     <Link to="acc-section" offset={-80}><li onClick={showNav}>ACCOMMODATION</li></Link>
                     <Link to="team-nav" offset={-80}><li onClick={showNav}>CONTACT</li></Link>
@@ -58,8 +59,31 @@ function Nav() {
             <button className='nav-button' onClick={showNav}>
                 <GiHamburgerMenu />
             </button>
+
+            
+
+
+
+            {isModalVisible && (
+                <div className="modal-overlay" onClick={toggleModal}>
+                     <div className="yeldivlin">
+                                        <img src={image} className="image" />
+                                    </div>
+                    <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+                        <button className="modal-close" onClick={toggleModal}>✕</button>
+                        <button className="modal-button">Gallery</button>
+                        <button className="modal-button">Report</button>
+                    </div>
+                     <div className='yeldivlin'>
+                                        <img src={image} className="image" />
+                                    </div>
+                </div>
+            )}
         </nav>
     );
 }
 
 export default Nav;
+                        
+                 
+
