8979d8779ae22cbf5316ee15ca0940597add8a6f
Abdullah <abdullahaziz723@gmail.com>
2025-01-18 08:10:02 +0530

    router-correct' '

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router,Route, Routes } from "react-router-dom"
+import { HashRouter as Router,Route, Routes } from "react-router-dom"
 import App from "./App"
 import Nav from "./Components/Navbar/nav"
 import Gallery_Section from "./Pages/Gallery_Section/Gallery_Section"
