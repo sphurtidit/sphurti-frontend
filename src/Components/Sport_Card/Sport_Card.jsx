6fca5a2f701fa1c8c33146bf36246a237cf79acf
Ashi-Verma1758 <ashiverma1758@gmail.com>
2025-01-25 15:31:20 +0530

    Sports Modal

--- src/Components/Sport_Card/Sport_Card.jsx
+++ src/Components/Sport_Card/Sport_Card.jsx
@@ -1,6 +1,30 @@
 import React from "react";
 import "./Sport_Card.css";
+import badmintonimg from "../../assets/badminton.png";
+import cricketimg from "../../assets/cricket.png";
+import footballimg from "../../assets/football.png";
+import volleyballimg from "../../assets/volleyball.png";
+import basketballimg from "../../assets/basketball.png";
+import tabletennisimg from "../../assets/table tennis.png";
 
+  const getImageForEvent = (eventName) => {
+    switch (eventName.toLowerCase()) {
+      case "badminton":
+        return badmintonimg;
+      case "cricket":
+        return cricketimg;
+      case "football":
+        return footballimg;
+      case "volleyball":
+        return volleyballimg;
+      case "basketball":
+        return basketballimg;
+      case "table tennis":
+        return tabletennisimg;
+      default:
+        return "";
+    }
+  };
 const SportsCard = ({ isOpen, onClose, gameDetails = {}, category = {} , eventName}) => {
   if (!isOpen) return null;
 
@@ -14,6 +38,7 @@ const SportsCard = ({ isOpen, onClose, gameDetails = {}, category = {} , eventNa
     maxNumber = "N/A",
   } = category;
   console.log('details-> ', category)
+  const eventImage = getImageForEvent(eventName);
 
   return (
     <div className="modal-overlay">
@@ -21,27 +46,32 @@ const SportsCard = ({ isOpen, onClose, gameDetails = {}, category = {} , eventNa
         <button className="close-button" onClick={onClose}>
           &times;
         </button>
-        <div className="modal-content">
+        <div className="borcontent">
           {/* Left Section */}
           <div className="left-section">
             <h1>{eventName}</h1>
-            <div className="fees-section">
+            <div className="fees-section"><div className="fe">
               <span className="label">Fees</span>
-              <span className="per-team">Per Team</span>
+              <span className="per-team">Per Team</span></div>
               <span className="amount">&#8377;{registrationFees}</span>
             </div>
             <div className="details">
-              <p>
-                <strong>Prize Money:</strong> <br />
-                Winners: &#8377;{prizeWinner} &nbsp; <span className="pipe">|</span> &nbsp;
+              <div >
+                <p className="strong">Prize Money:</p> <br />
+                Winners: &#8377;{prizeWinner} &nbsp; 
+                <span className="pipe">|</span> &nbsp;
                 Runner Up: &#8377;{prizeRunnerUp}
-              </p>
+              </div>
               <p>
                 <strong>Team Size:</strong> {minNumber} - {maxNumber} players
               </p>
+              <div className="details">
+              <p className="strong">Schedule: <a href="" className="amt">Download Schedule</a></p>
+              <p className="strong">Rule Book:  <a href="" className="amt">Download</a></p>
+              </div>
             </div>
             <div className="button-group">
-              <button className="register">Register</button>
+              <button className="results">Register</button>
               <button className="results">Results</button>
             </div>
           </div>
@@ -49,9 +79,9 @@ const SportsCard = ({ isOpen, onClose, gameDetails = {}, category = {} , eventNa
           {/* Right Section */}
           <div className="right-section">
             <img
-              src="basketball.png"
+              src={eventImage}
               alt={`${categoryName || "Game"} Player`}
-              className="image"
+              className="spimg"
             />
           </div>
         </div>
