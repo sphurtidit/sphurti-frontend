75d25c352144b0ec2b95872da48f2a0122f0f15d
Saaj03 <sarfarajahamed856@gmail.com>
2025-02-12 22:50:26 +0530

    Create RulesSection

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,5 @@
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import App from "./App";
-// import Nav from "./Components/Navbar/nav";
 import Gallery_Section from "./Pages/Gallery_Section/Gallery_Section";
 import DevelopersPage from "./Pages/Developers Page/developers";
 import Loginpage from "./Components/Login/Loginpage";
@@ -11,27 +10,27 @@ import RegistrationForm from "./Components/Registration_Page/Registration_Page1"
 import TeamRegistration from "./Components/Registration_Page/Registration_Page2";
 import FAQPage from "./Components/FAQ/FAQ";
 import ResultTable from "./Components/Result_Table/Result_Table";
+import RulesSection from "./Components/GeneralRules/GeneralRules_Page";
 
 export const Home = () => {
   return (
-    <main>
-      <Router>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/gallery" element={<Gallery_Section />} />
-          <Route path="/developers" element={<DevelopersPage />} />
-          <Route path="/Loginpage" element={<Loginpage />} />
-          <Route path="/Signinpage" element={<Signinpage />} />
-          <Route path="/comingsoon" element={<ComingSoon />} />
-          <Route path="/Reg_Pg1" element={<RegistrationForm />} />
-          <Route path="/Reg_Pg2" element={<TeamRegistration />} />
-          <Route path="/FAQ" element={<FAQPage />} />
-          <Route path="/ResultTable" element={<ResultTable />} />
-          <Route path="/profilepage" element={<ProfilePage />} />
-
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/gallery" element={<Gallery_Section />} />
+        <Route path="/developers" element={<DevelopersPage />} />
+        <Route path="/Loginpage" element={<Loginpage />} />
+        <Route path="/Signinpage" element={<Signinpage />} />
+        <Route path="/comingsoon" element={<ComingSoon />} />
+        <Route path="/Reg_Pg1" element={<RegistrationForm />} />
+        <Route path="/Reg_Pg2" element={<TeamRegistration />} />
+        <Route path="/FAQ" element={<FAQPage />} />
+        <Route path="/ResultTable" element={<ResultTable />} />
+        <Route path="/profilepage" element={<ProfilePage />} />
         
-        </Routes>
-      </Router>
-    </main>
+        <Route path="/RulesSection" element={<RulesSection />} />
+
+      </Routes>
+    </Router>
   );
 };
