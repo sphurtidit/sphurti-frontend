183ab871492b8aad097a5c81eb9c55579eeda088
SPARK9497 <shreyanshgupta9497@gmail.com>
2025-02-14 11:05:06 +0530

    nav

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,44 +1,49 @@
-import { MenuIcon, X } from "lucide-react";
+import { Menu, X } from "lucide-react";
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import DIT from "../../assets/DIT.png";
 import NAAC from "../../assets/naac.png";
 import SPHURTI from "../../assets/sphurti.png";
 import SPH from "../../assets/sph.png";
 import "./Navbar.css";
-import { Link } from "react-router-dom";
 
 function Navbar() {
   const [hamburgerOpen, setHamburgerOpen] = useState(false);
-  console.log(hamburgerOpen);
+
   return (
     <div>
       <div className="nav-container">
         <div>
           <img src={SPH} alt="SPH Logo" className="logo" />
         </div>
+
         <div>
           <img src={NAAC} alt="NAAC Logo" className="logo" />
         </div>
+
+        {/* Login Button (Placed between NAAC logo and Hamburger Icon) */}
+        <Link to="/login" className="login-btn">Login</Link>
+
+        {/* Hamburger Icon */}
         <span
           className="hamburger-icon"
           onClick={() => setHamburgerOpen((prev) => !prev)}
         >
-          {hamburgerOpen ? <X size={32} /> : <MenuIcon size={32} />}
+          {hamburgerOpen ? <X size={32} /> : <Menu size={32} />}
         </span>
       </div>
+
+      {/* Dropdown Menu */}
       <div className={`dropdown ${hamburgerOpen ? "show" : ""}`}>
         <Link to="/">HOME</Link>
         <Link to="/">SPORTS</Link>
         <Link to="#">GENERAL RULES</Link>
         <Link to="#">ORGANISING LEADERS</Link>
-        <Link to="#">AMABASSADORS</Link>
+        <Link to="#">AMBASSADORS</Link>
         <Link to="/FAQ">FAQ</Link>
         <Link to="#">ARCHIVE</Link>
         <Link to="#">CONTACT US</Link>
-        
-        
       </div>
-      
     </div>
   );
 }
