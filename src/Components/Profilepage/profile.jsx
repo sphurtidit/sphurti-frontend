7fc2699faef7bb4f9c47e3d62febc520c44d5d35
tanya78811 <131523834+tanya78811@users.noreply.github.com>
2025-02-14 18:33:52 +0530

    fetchuser

--- src/Components/Profilepage/profile.jsx
+++ src/Components/Profilepage/profile.jsx
@@ -5,9 +5,12 @@ import { useNavigate } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+import useUserStore from '../../store/userStore';
+
 function ProfilePage() {
-  const [userDetails, setUserDetails] = useState(null); // State to store user details
-  const [error, setError] = useState(null); // State to store any error
+  const { user, fetchUser, logout } = useUserStore();
+  //const [userDetails, setUserDetails] = useState(null); // State to store user details
+  // const [error, setError] = useState(null); // State to store any error
   const navigate = useNavigate();
 
   const handleNavigation = (path) => {
@@ -16,14 +19,14 @@ function ProfilePage() {
 
   useEffect(() => {
     fetchUser();
-  }, []);
+  }, [fetchUser]);
 
-  if (error) {
-    return <div className={profile.error}>{error}</div>;
-  }
+  // if (error) {
+  //   return <div className={profile.error}>{error}</div>;
+  // }
 
   // Display a loading state while fetching data
-  if (!userDetails) {
+  if (!user) {
     return <div className={profile.loading}>Loading...</div>;
   }
 
@@ -38,7 +41,7 @@ function ProfilePage() {
           <button
             className="modal-button"
             onClick={() => {
-              localStorage.removeItem("authToken");
+              logout();
               handleNavigation("/");
               toast.info("Logged out successfully.");
             }}
@@ -64,19 +67,19 @@ function ProfilePage() {
           </div>
           <div className={profile.bgplate1}>
             <h2 className={profile.text}>
-              College: {userDetails.college_name}
+              College: {user.college_name}
             </h2>
           </div>
           <div className={profile.bgplate}>
             <h2 className={profile.text}>
-              College ID: {userDetails.college_id}
+              College ID: {user.college_id}
             </h2>
           </div>
           <div className={profile.bgplate1}>
-            <h2 className={profile.text}>Branch: {userDetails.branch}</h2>
+            <h2 className={profile.text}>Branch: {user.branch}</h2>
           </div>
           <div className={profile.bgplate}>
-            <h2 className={profile.text}>Year: {userDetails.year}</h2>
+            <h2 className={profile.text}>Year: {user.year}</h2>
           </div>
         </div>
       </div>
