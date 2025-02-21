f6a42be6d5804bf73c9f4a1a6b73c80eaffe3d5d
Saaj03 <sarfarajahamed856@gmail.com>
2025-02-21 22:04:45 +0530

    Responsiveness of Ambassador page

--- src/Components/AMBASSADORS/AMBASSADORS_page.jsx
+++ src/Components/AMBASSADORS/AMBASSADORS_page.jsx
@@ -3,33 +3,32 @@ import './AMBASSADORS_page.css';
 import Ambassadors1 from "../../assets/Gallery/Ambassadors1.png";
 import Ambassadors2 from "../../assets/Gallery/Ambassadors2.png";
 import Nav from '../Navbar/nav';
+import Footer from "../Footer/Footer";
 
 const ambassadorsData = [
   {
     name: "Vibhor Talwar",
-    year: "4th Year",
     course: "BTech CSE",
     imageSrc: Ambassadors1,
     achievements: [
-      "Represented India at the 11th World Karate Championships, held at Santiago, Chile in 2019 and ranked 11th in my category.",
-      "Achieved 1st place at All India University Games 2021-22 held at Kurukshetra University, Haryana in the senior category men under 67kg.",
-      "Represented DIT in Khelo India University Games 2022, held at Bangalore, Karnataka in discipline karate and secured 3rd place.",
-      "9x National champion and 13x State Champion.",
-      "Awarded the scholarship of 3 lac rupees from Delhi Govt. in 2019 for multiple achievements in Karate."
+      "• Represented India at the 11th World Karate Championships, held at Santiago, Chile in 2019 and ranked 11th in my category.",
+      "• Achieved 1st place at All India University Games 2021-22 held at Kurukshetra University, Haryana in the senior category men under 67kg.",
+      "• Represented DIT in Khelo India University Games 2022, held at Bangalore, Karnataka in discipline karate and secured 3rd place.",
+      "• Awarded the scholarship of 3 lac rupees from Delhi Govt. in 2019 for multiple achievements in Karate.",
+      "• 9x National champion and 13x State Champion.",
     ],
     colorClass: "red-bg",
   },
   {
     name: "Nikita Singh",
-    year: "3rd Year",
     course: "BTech CSE",
     imageSrc: Ambassadors2,
     achievements: [
-      "All India nationals karate championship.",
-      "Bronze medal 2024 at Haryana Olympic Bhawan Panchkula, organized by Karate Association of India (KAI).",
-      "Ghaziabad district gold 2024 and silver 2022.",
-      "Uttar Pradesh state karate gold 2024 at GLA university, organized by Uttar Pradesh state karate association (UPSKA).",
-      "1 open international medal and 12+ state and inter-state medals and local championship medals."
+      "• All India nationals karate championship.",
+      "• Bronze medal 2024 at Haryana Olympic Bhawan Panchkula, organized by Karate Association of India (KAI).",
+      "• Ghaziabad district gold 2024 and silver 2022.",
+      "• Uttar Pradesh state karate gold 2024 at GLA university, organized by Uttar Pradesh state karate association (UPSKA).",
+      "• 1 open international medal and 12+ state and inter-state medals and local championship medals."
     ],
     colorClass: "blue-bg",
   },
@@ -46,15 +45,13 @@ const AmbassadorsPage = () => {
             <div key={index} className={`ambassador-card ${ambassador.colorClass}`}>
               <div className="image-section">
                 <img src={ambassador.imageSrc} alt={ambassador.name} className="ambassador-image" />
-                <div className="ambassador-info">
-                  <p className="ambassador-name">{ambassador.name}</p>
-                  <p>{ambassador.year}</p>
-                  <p>{ambassador.course}</p>
-                </div>
               </div>
               <div className="achievements-section">
-                <h3>ACHIEVEMENTS</h3>
-                <ul>
+                <div className="ambassador-info right-align">
+                  <p className="ambassador-name">{ambassador.name}</p>
+                  <p className="ambassador-course">{ambassador.course}</p>
+                </div>
+                <ul className="achievements-list">
                   {ambassador.achievements.map((achievement, i) => (
                     <li key={i}>{achievement}</li>
                   ))}
@@ -64,6 +61,7 @@ const AmbassadorsPage = () => {
           ))}
         </div>
       </section>
+      <Footer/>
     </div>
   );
 };
