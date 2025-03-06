32684c27763bf4286e85af1acbaa14907cf0c9bb
TheDarkLord100 <miracleyt404@gmail.com>
2025-03-06 11:39:47 +0530

    Added Carousel

--- src/Components/Sport_Card/Sport_Card.jsx
+++ src/Components/Sport_Card/Sport_Card.jsx
@@ -89,14 +89,14 @@ const SportsCard = ({
               <button
                 className="card-results"
                 onClick={() => navigateToRegistraion()}
-                
+                disabled={name === "Table Tennis "}
               >
                 Register
               </button>
               <button
                 className="card-results"
                 onClick={() => openRules(rules)}
-                
+                disabled={name === "Table Tennis "}
               >
                 Rules
               </button>
